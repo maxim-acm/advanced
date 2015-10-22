32db98d03cfa910ca329ac751770b2ccbcf79158
koorchik <koorchik@gmail.com>
2015-10-22 21:08:46 +0300

    Render harcoded properties list

--- lesson6/nestoria_handlebars/js/main.js
+++ lesson6/nestoria_handlebars/js/main.js
@@ -3,7 +3,21 @@ $(document).ready(function() {
     var data = {
         location: DATA.request.location,
         numberOfProperties: DATA.response.listings.length,
-        searchDate: DATA.response.created_http
+        searchDate: DATA.response.created_http,
+        properties: [
+            {
+                title: 'Property 1',
+                price: 10,
+                currency: 'USD',
+                imgURL: 'http://1.l.uk.nestoria.nestimg.com/lis/3/7/b/2ddbe2e5b1f0bcf52f270f79d254cb1faad46.2.jpg'
+            },
+            {
+                title: 'Property 1',
+                price: 10,
+                currency: 'USD',
+                imgURL: 'http://1.l.uk.nestoria.nestimg.com/lis/3/7/b/2ddbe2e5b1f0bcf52f270f79d254cb1faad46.2.jpg'
+            }
+        ]
     };
 
     var rendered = renderTemplate('entry-template', data);
