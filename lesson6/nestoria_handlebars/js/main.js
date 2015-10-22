da0a732f7a7661b785039ead4e57ef4985ba7054
koorchik <koorchik@gmail.com>
2015-10-22 21:41:35 +0300

    Add niceDate helper

--- lesson6/nestoria_handlebars/js/main.js
+++ lesson6/nestoria_handlebars/js/main.js
@@ -1,29 +1,27 @@
 $(document).ready(function() {
+    Handlebars.registerHelper('niceDate', function(unixTime) {
+        return moment.unix(unixTime).fromNow();
+    });
 
     var data = {
         location: DATA.request.location,
         numberOfProperties: DATA.response.listings.length,
-        searchDate: DATA.response.created_http,
-        properties: [
-            {
-                title: 'Property 1',
-                price: 10,
-                currency: 'USD',
-                imgURL: 'http://1.l.uk.nestoria.nestimg.com/lis/3/7/b/2ddbe2e5b1f0bcf52f270f79d254cb1faad46.2.jpg'
-            },
-            {
-                title: 'Property 1',
-                price: 10,
-                currency: 'USD',
-                imgURL: 'http://1.l.uk.nestoria.nestimg.com/lis/3/7/b/2ddbe2e5b1f0bcf52f270f79d254cb1faad46.2.jpg'
-            }
-        ]
+        searchDate: DATA.response.created_unix,
+        properties: DATA.response.listings.map(formatProperty)
     };
 
     var rendered = renderTemplate('entry-template', data);
     $('#content').html(rendered);
 });
 
+function formatProperty(originalProperty) {
+    return {
+        title: originalProperty.title,
+        price: originalProperty.price,
+        currency: originalProperty.price_currency,
+        imgURL: originalProperty.img_url
+    };
+}
 
 function renderTemplate(templateName, data) {
     var templateString = $('#' + templateName).html();
